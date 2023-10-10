28c1df01999de02fee0dcd0b475006064cbbcbce
SeoMinJong <65899572+SeoMinJong@users.noreply.github.com>
2023-10-10 23:52:55 +0900

    REST API 형태 동적 server

---
+++ express_server_dynamic.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+let posts = [];
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req, res)=>{
+    res.json(posts);
+});
+
+app.post("/posts", (req, res)=>{
+    const {title, name, text} = req.body;
+
+    posts.push({id: posts.length+1, title, name, text, created: Date()});
+    res.json({title, name, text});
+});
+
+app.delete("/posts/:id", (req, res)=>{
+    const id = req.params.id;
+    const filteredPosts = posts.filter((post) => post.id !== +id);
+    const isLengthChanged = posts.length !== filteredPosts.length;
+    posts = filteredPosts;
+
+    if(isLengthChanged){
+        res.jons("OK");
+        return;
+    }
+    
+    res.json("NOT CHANGE");
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Start SERVER : use ${PORT}`);
+});
