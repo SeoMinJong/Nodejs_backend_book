86d8d41806bd4cd58d630486c3b725eb2e3dc446
SeoMinJong <65899572+SeoMinJong@users.noreply.github.com>
2023-10-16 00:15:35 +0900

    catch와  finally를 통한 오류 검출 및 마무리 처리

--- promise-test.js
+++ promise-test.js
@@ -1,19 +1,19 @@
 const DB=[];
 
 function saveDB(user){
-    const oldDBSize = DB.length;
+    const oldDBSize = DB.length + 1;
     DB.push(user);
     console.log(`save ${user.name} to DB`);
     return new Promise((resolve, reject)=>{
         if (DB.length > oldDBSize){
             resolve(user)
         }else{
-            console.log(new Error("Save DB ERROR!"))
+            reject(new Error("Save DB ERROR!"));
         }
     });
 };
 
-function sendEmail(user, callback){
+function sendEmail(user){
     console.log(`email to ${user.email}`);
     return new Promise((resolve)=>{
         resolve(user);
@@ -27,14 +27,16 @@ function getResult(user){
 };
 
 function registerByPromise(user){
-    const result = saveDB(user).then(sendEmail).then(getResult);
+    const result = saveDB(user).then(sendEmail).then(getResult)
+        .catch(error => new Error(error))
+        .finally(() => console.log("end!"));
     console.log(result);
     return result;
-}
+};
 
 const myUser = {email:"andy@naver.com", password:"1234", name:"andy"};
-// const result = registerByPromise(myUser);
+const result = registerByPromise(myUser);
 // result.then(console.log);
 
-allResult = Promise.all([saveDB(myUser), sendEmail(myUser), getResult(myUser)]);
-allResult.then(console.log);
+// allResult = Promise.all([saveDB(myUser), sendEmail(myUser), getResult(myUser)]);
+// allResult.then(console.log);
