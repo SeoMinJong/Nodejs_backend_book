20da38b7515f9a3ce33caddd4cba8633fab4fe9d
SeoMinJong <65899572+SeoMinJong@users.noreply.github.com>
2023-10-24 15:56:09 +0900

    write page 작성

--- project1/app.js
+++ project1/app.js
@@ -7,7 +7,11 @@ app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
 
 app.get("/", (req, res)=>{
-    res.render("home", { title : "테스트 게시판", message : "만나서 반갑!"});
+    res.render("home", { title : "테스트 게시판"});
 });
+
+app.get("/write", (req, res)=>{
+    res.render("write", {title: "테스트 게시판"})
+})
 
 app.listen(3000);
