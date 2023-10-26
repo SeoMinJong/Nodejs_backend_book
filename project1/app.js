c54d6e65757268674bcd60d616d45d0ea936e9fc
SeoMinJong <65899572+SeoMinJong@users.noreply.github.com>
2023-10-26 18:11:18 +0900

    detail page - comment API

--- project1/app.js
+++ project1/app.js
@@ -100,6 +100,32 @@ app.delete("/delete", async (req, res)=>{
     
 })
 
+app.post("/write-comment", async (req, res)=>{
+    const { id, name, password, comment } = req.body;
+    const post = await postService.getPostById(collection, id);
+
+    if (post.comments){
+        post.comments.push({
+            idx: post.comments.length + 1,
+            name,
+            password,
+            comment,
+            createdDt: new Date().toString(),
+        });
+    }else{
+        post.comments=[{
+            idx:1,
+            name,
+            password,
+            comment,
+            createdDt: new Date().toString(),
+        }]
+    }
+
+    await postService.updatePost(collection, id, post);
+    return res.redirect(`detail/${id}`);
+})
+
 app.listen(3000, async()=>{
     console.log("Server Started");
     const mongodbClient = await mongodbConnection();
