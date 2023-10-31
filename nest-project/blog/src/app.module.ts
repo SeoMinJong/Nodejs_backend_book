9785a377e3348261dea751298a455c6b9bdcc383
SeoMinJong <65899572+SeoMinJong@users.noreply.github.com>
2023-10-31 14:46:46 +0900

    Dependency Injection

--- nest-project/blog/src/app.module.ts
+++ nest-project/blog/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BlogContoller } from './blog.contoller';
 import { BlogService } from './blog.service';
+import { BlogFileRepository } from './blog.repository';
 
 @Module({
   imports: [],
   controllers: [BlogContoller],
-  providers: [BlogService],
+  providers: [BlogService, BlogFileRepository],
 })
 export class AppModule {}
