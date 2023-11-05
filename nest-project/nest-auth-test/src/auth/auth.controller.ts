b9ddefaebf3ca2f72f7bcd8501a96e865a261641
SeoMinJong <65899572+SeoMinJong@users.noreply.github.com>
2023-11-05 16:08:24 +0900

    auth login

--- nest-project/nest-auth-test/src/auth/auth.controller.ts
+++ nest-project/nest-auth-test/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request, Response } from '@nestjs/common';
 import { CreateUserDto } from 'src/user/user.dto';
 import { AuthService } from './auth.service';
 
@@ -9,5 +9,22 @@ export class AuthController {
     @Post('register')
     async register(@Body() userDto: CreateUserDto){
         return await this.authService.register(userDto);
+    }
+
+    @Post('login')
+    async login(@Request() req, @Response() res){
+        console.log(req.body)
+        const userInfo = await this.authService.validateUser(
+            req.body.email,
+            req.body.password,
+        );
+        
+        if(userInfo){
+            res.cookie('login', JSON.stringify(userInfo)),{
+                httpOnly: false,
+                maxAge: 1000*660*60*24*7
+            }
+        }
+        return res.send({message: 'login success'})
     }
 }
